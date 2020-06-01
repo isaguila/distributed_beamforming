1c49c25aa26abca313730001381fdb3b0333475b
Ivan Aguilar <ivan@MacBook-Pro.local>
2020-05-31 23:51:28 -0700

    fixed demod_sig() input "scale_factor" to accept scalar or vector type

--- demodulator/qam_demodulator.m
+++ demodulator/qam_demodulator.m
@@ -9,7 +9,7 @@ classdef qam_demodulator
             obj.M = M;
         end
         function obj = demod_sig(obj, x, scale_factor)
-            obj.syms = x*scale_factor/norm(x);
+            obj.syms = x*diag(scale_factor./vecnorm(x));
             obj.bits = qamdemod(obj.syms, obj.M, 'OutputType', 'bit', 'UnitAveragePower', true);
         end
     end
