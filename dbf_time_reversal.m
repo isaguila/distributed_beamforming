d5c41e048559447fe8004137f807872e25fd0c23
Ivan Aguilar <ivan@MacBook-Pro.local>
2020-06-07 03:43:36 -0700

    added comparisons to dbf non TR and SISO

--- dbf_time_reversal.m
+++ dbf_time_reversal.m
@@ -14,9 +14,9 @@ rng(0);
 % by P. Kyritsi, G. Papanicolaou, P. Eggers, A. Oprea
 %--------------------------------------------------------------------------
 
-Ntx = 4;
+Ntx = 10;
 M = 4;
-snr_db = 20;
+snr_db = 15;
 
 mod_block = qam_modulator(M);
 demod_block = qam_demodulator(M);
@@ -26,9 +26,17 @@ scale_factor = vecnorm(mod_block.syms).';
 tx_tr = struct();
 rx_tr = struct();
 
-tx_tr.sig    = repmat(mod_block.syms, 1, Ntx);
+tx_bl = struct();
+rx_bl = struct();
 
-L = 4000;
+tx_ntr = struct();
+rx_ntr = struct();
+
+tx_tr.sig  = repmat(mod_block.syms, 1, Ntx);
+tx_ntr.sig = repmat(mod_block.syms, 1, Ntx);
+tx_bl.sig  = mod_block.syms;
+
+L = 1e4;
 h = complex(zeros(L, Ntx));
 h_tr = complex(zeros(L, Ntx));
 
@@ -42,18 +50,53 @@ for ii = 1:Ntx
      
     % Apply channel
     tx_tr.sig(:, ii) = conv(tx_tr.sig(:, ii), h(:, ii), 'same');
+    tx_ntr.sig(:, ii) = conv(tx_ntr.sig(:, ii), h(:, ii), 'same');
 end
- 
 
+% Choose a channel for baseline SISO and apply it
+h_bl = randn(L, 2)*[1; 1i];
+tx_bl.sig = conv(tx_bl.sig, h_bl, 'same');
+
+ 
 sig_len = length(tx_tr.sig);
-p_sig = mean(vecnorm(tx_tr.sig).^2);
+p_sig = mean(vecnorm(tx_tr.sig).^2)/sig_len;
 p_noise = p_sig / 10^(snr_db);
 noise = sqrt(p_noise/2)*randn(sig_len, 2)*[1; 1i];
 rx_tr.sig = sum(tx_tr.sig, 2) + noise;
 
+sig_len = length(tx_ntr.sig);
+p_sig = mean(vecnorm(tx_ntr.sig).^2)/sig_len;
+p_noise = p_sig / 10^(snr_db);
+noise = sqrt(p_noise/2)*randn(sig_len, 2)*[1; 1i];
+rx_ntr.sig = sum(tx_ntr.sig, 2) + noise;
 
+sig_len = length(tx_bl.sig);
+p_sig = mean(vecnorm(tx_bl.sig).^2)/sig_len;
+p_noise = p_sig / 10^(snr_db);
+noise = sqrt(p_noise/2)*randn(sig_len, 2)*[1; 1i];
+rx_bl.sig = sum(tx_bl.sig, 2) + noise;
+
+% phi = angle(rx_tr.sig.*conj(mod_block.syms)
+% phi = angle(sum(rx_tr.sig.*conj(mod_block.syms)));
+% rx_tr.sig = rx_tr.sig.*exp(-1i*phi);
 demod_block = demod_block.demod_sig(rx_tr.sig, scale_factor);
 rx_tr.syms = reshape(demod_block.syms, [], 1);
+rx_tr.evm = calc_evm(rx_tr.syms, M);
+rx_tr.bits = demod_block.bits;
+rx_tr.ber = sum(bitxor(rx_tr.bits, mod_block.bits));
+
+demod_block = demod_block.demod_sig(rx_ntr.sig, scale_factor);
+rx_ntr.syms = reshape(demod_block.syms, [], 1);
+rx_ntr.evm = calc_evm(rx_ntr.syms, M);
+rx_ntr.bits = demod_block.bits;
+rx_ntr.ber = sum(bitxor(rx_ntr.bits, mod_block.bits));
+
+demod_block = demod_block.demod_sig(rx_bl.sig, scale_factor);
+rx_bl.syms = reshape(demod_block.syms, [], 1);
+rx_bl.evm = calc_evm(rx_bl.syms, M);
+rx_bl.bits = demod_block.bits;
+rx_bl.ber = sum(bitxor(rx_bl.bits, mod_block.bits));
+
 figure();
 plot(real(rx_tr.syms), imag(rx_tr.syms), '.', 'MarkerSize', 10);
 title('Time Reversal');
@@ -61,3 +104,24 @@ grid on;
 xlim([-2, 2]);
 ylim([-2, 2]);
 axis square;
+
+figure();
+plot(real(rx_ntr.syms), imag(rx_ntr.syms), '.', 'MarkerSize', 10);
+title('Time Reversal');
+grid on;
+xlim([-2, 2]);
+ylim([-2, 2]);
+axis square;
+
+figure();
+plot(real(rx_bl.syms), imag(rx_bl.syms), '.', 'MarkerSize', 10);
+title('Baseline');
+grid on;
+xlim([-2, 2]);
+ylim([-2, 2]);
+
+disp(rx_tr.evm);
+disp(rx_ntr.evm);
+disp(rx_bl.evm);
+
+% scatterplot(awgn(qammod(randi([0 M-1], 1000, 1), M, 'UnitAveragePower', true), -rx_tr.evm, 'measured'))
